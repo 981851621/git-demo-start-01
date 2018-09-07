84ed78c3223373344aa506dcee2af57bbf84d42e
mb <18820147556@139.com>
2018-09-07 20:12:01 +0800

    在dev分支 上开发并首次提交，版本2.0

--- git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
+++ git-demo-01-start/src/cn/gzsxt/git/test/TestGit.java
@@ -4,5 +4,11 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("this is my first project share to git");
+		
+		say("hello");
+	}
+	
+	public static void say(String word) {
+		System.out.println(word);
 	}
 }
